1aa718e4d452da7d8d9bf4c778f315914a78de31
Serhan KARAKOÇ <108010261+serhankarakoc@users.noreply.github.com>
2022-07-14 10:55:48 +0300

    Add files via upload

---
+++ main.go
@@ -0,0 +1,47 @@
+// 🚀 Fiber is an Express inspired web framework written in Go with 💖
+// 📌 API Documentation: https://docs.gofiber.io
+// 📝 Github Repository: https://github.com/gofiber/fiber
+
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/websocket/v2"
+)
+
+func main() {
+	app := fiber.New()
+
+	// Optional middleware
+	app.Use("/ws", func(c *fiber.Ctx) error {
+		if c.Get("host") == "localhost:3000" {
+			c.Locals("Host", "Localhost:3000")
+			return c.Next()
+		}
+		return c.Status(403).SendString("Request origin not allowed")
+	})
+
+	// Upgraded websocket request
+	app.Get("/ws", websocket.New(func(c *websocket.Conn) {
+		fmt.Println(c.Locals("Host")) // "Localhost:3000"
+		for {
+			mt, msg, err := c.ReadMessage()
+			if err != nil {
+				log.Println("read:", err)
+				break
+			}
+			log.Printf("recv: %s", msg)
+			err = c.WriteMessage(mt, msg)
+			if err != nil {
+				log.Println("write:", err)
+				break
+			}
+		}
+	}))
+
+	// ws://localhost:3000/ws
+	log.Fatal(app.Listen(":3000"))
+}
