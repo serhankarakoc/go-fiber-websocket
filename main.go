53f324c7df1421eccb4696e111f11845608c9f06
Serhan KARAKOÇ <108010261+serhankarakoc@users.noreply.github.com>
2022-07-14 11:18:31 +0300

    Update main.go

--- main.go
+++ main.go
@@ -1,7 +1,3 @@
-// 🚀 Fiber is an Express inspired web framework written in Go with 💖
-// 📌 API Documentation: https://docs.gofiber.io
-// 📝 Github Repository: https://github.com/gofiber/fiber
-
 package main
 
 import (
